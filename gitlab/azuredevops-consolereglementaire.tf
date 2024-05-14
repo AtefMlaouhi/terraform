32f3b8349bb8b5dc08f296fe0db177039db2c5f8
Vincent De Oliveira <vincent-de-oliveira@harvest.fr>
2024-05-14 09:57:27 +0000

    azuredevops: add GitlabDockerRegistrySemanticRelease to ConsoleReglementaire

--- gitlab/azuredevops-consolereglementaire.tf
+++ gitlab/azuredevops-consolereglementaire.tf
@@ -25,3 +25,20 @@ resource "azuredevops_resource_authorization" "docker-gitlab-consolereglementair
   resource_id = azuredevops_serviceendpoint_dockerregistry.docker-gitlab-consolereglementaire.id
   authorized  = true
 }
+
+# create the connection between gitlab semanticrelease <-> ConsoleReglementaire project
+resource "azuredevops_serviceendpoint_dockerregistry" "docker-gitlab-semanticrelease-consolereglementaire" {
+  project_id            = data.azuredevops_project.consolereglementaire.id
+  service_endpoint_name = "GitlabDockerRegistrySemanticRelease"
+  docker_registry       = "https://registry-git.harvest.fr/"
+  docker_username       = gitlab_deploy_token.semantic-release-registry.username
+  docker_password       = gitlab_deploy_token.semantic-release-registry.token
+  registry_type         = "Others"
+}
+
+# # authorize the project ConsoleReglementaire to use the connection semanticrelease
+resource "azuredevops_resource_authorization" "docker-gitlab-semanticrelease-consolereglementaire" {
+  project_id  = data.azuredevops_project.consolereglementaire.id
+  resource_id = azuredevops_serviceendpoint_dockerregistry.docker-gitlab-semanticrelease-consolereglementaire.id
+  authorized  = true
+}
