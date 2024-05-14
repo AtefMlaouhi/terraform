29e337f5de7f8a5390dacd35267c0e7cbdc63a93
Vincent De Oliveira <vincent-de-oliveira@harvest.fr>
2024-05-14 08:20:00 +0000

    azuredevops: add consolereglementaire

---
+++ gitlab/azuredevops-consolereglementaire.tf
@@ -0,0 +1,27 @@
+# docker push token
+resource "gitlab_deploy_token" "consolereglementaire-argocd-dotnet-registry" {
+  name    = "azuredevops-consolereglementaire"
+  scopes  = ["read_registry", "write_registry"]
+  project = data.gitlab_project.argocd-dotnet.id
+}
+
+data "azuredevops_project" "consolereglementaire" {
+  name = "ConsoleReglementaire"
+}
+
+# create the connection between gitlab <-> ConsoleReglementaire project
+resource "azuredevops_serviceendpoint_dockerregistry" "docker-gitlab-consolereglementaire" {
+  project_id            = data.azuredevops_project.consolereglementaire.id
+  service_endpoint_name = "Gitlab Quantalys registry"
+  docker_registry       = "https://registry-git.harvest.fr/"
+  docker_username       = gitlab_deploy_token.consolereglementaire-argocd-dotnet-registry.username
+  docker_password       = gitlab_deploy_token.consolereglementaire-argocd-dotnet-registry.token
+  registry_type         = "Others"
+}
+
+# authorize the project ConsoleReglementaire to use the connection
+resource "azuredevops_resource_authorization" "docker-gitlab-consolereglementaire" {
+  project_id  = data.azuredevops_project.consolereglementaire.id
+  resource_id = azuredevops_serviceendpoint_dockerregistry.docker-gitlab-consolereglementaire.id
+  authorized  = true
+}
