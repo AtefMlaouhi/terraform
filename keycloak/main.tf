3a6f80d11843ef16b9e9e90fcbd383bf9b0412cc
ygeng <yu.geng@harvest.fr>
2024-06-14 14:34:52 +0200

    initier le terraform pour axalux

--- keycloak/main.tf
+++ keycloak/main.tf
@@ -6,7 +6,7 @@ terraform {
 
 provider "keycloak" {
   url       = "${var.KEYCLOAK_url}/auth"
-  client_id = "terraform-pw"
+  client_id = "terraform-client"
   #username  = var.KEYCLOAK_username
   client_secret = var.KEYCLOAK_password
 }
